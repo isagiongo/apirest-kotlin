d23fd3fd011f1bcae0f2c8914fab4f7c50ed7aed
Isa Giongo <isagiongo@gmail.com>
2019-05-25 18:31:39 -0300

    Cria teste para empresa service

--- src/main/kotlin/com/isagiongo/apirestkotlin/repositories/LancamentoRepository.kt
+++ src/main/kotlin/com/isagiongo/apirestkotlin/repositories/LancamentoRepository.kt
@@ -2,8 +2,8 @@ package com.isagiongo.apirestkotlin.repositories
 
 import com.isagiongo.apirestkotlin.documents.Lancamento
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
-import java.awt.print.Pageable
 
 interface LancamentoRepository : MongoRepository <Lancamento,String> {
     fun findByFuncionarioId(funcionarioId: String, pageable: Pageable): Page<Lancamento>
