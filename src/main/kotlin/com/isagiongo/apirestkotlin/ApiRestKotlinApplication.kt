d8928fbc57aca1343f8dd974b7c09ebe99544ba1
Isa Giongo <isagiongo@gmail.com>
2019-05-25 10:59:39 -0300

    Inicia projeto spring kotlin

---
+++ src/main/kotlin/com/isagiongo/apirestkotlin/ApiRestKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.isagiongo.apirestkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiRestKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiRestKotlinApplication>(*args)
+}
